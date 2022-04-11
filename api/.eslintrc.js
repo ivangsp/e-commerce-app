eda504793f97da7b218acd12fa644665bd704021
ivan <ivan.ojiambo@katanamrp.com>
2022-04-11 15:27:00 +0300

    Allow property name , application/json as a valid property name

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
   extends: '@loopback/eslint-config',
+  rules: {
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        // Allow property name , application/json as a valid property name
+        //Ref: https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/naming-convention.md
+        "selector": "property",
+        "format": ["strictCamelCase"],
+        "filter": {
+          // you can expand this regex to add more allowed names
+          "regex": "^(application#/json|Property-Name-Two)$",
+          "match": false
+        }
+      }
+    ]
+  }
 };
